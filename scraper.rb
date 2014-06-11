0eaad0780e6252595ba3f8539ae5127d7853ce43
Eumir Gaspar <eumir@aelogica.com>
2014-06-12 07:04:21 +0800

    Added thumbnails

--- scraper.rb
+++ scraper.rb
@@ -14,6 +14,7 @@ rows.each_with_index do |row, index|
   data << {
     id: index,
     stars: cols[0].xpath('span/text()').to_s,
+    thumbnail_url: cols[0].xpath('a/img/@src').to_s,
     text: cols[1].xpath('b/a/text()').to_s,
     cost: cols[3].text().strip,
     materials: cols[4].xpath('a').map { |a| a['title'] }
